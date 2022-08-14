9cac94f553ce623eae37ec76b0f618e8749a69c8
Joao Paulo Araujo <paulointer077@gmail.com>
2022-08-14 17:22:01 -0400

    Conexão com a API - Formatando valores

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { TransactionContext } from '../../contexts/TransectionsContext';
+import { dateformatter, priceFormatter } from '../../utils/formatted';
 import { SearchForm } from './components/SearchForm';
 import { PriceHighlight, TransactionsConnntainer, TransactionsTable } from './styles';
 
@@ -23,11 +24,12 @@ export function Transactions() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- ' }
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateformatter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
